c7713cc4f4b2292f523e5b8658fa18c98112ec98
Ananya Ritu <ananyaritu8@gmail.com>
2021-08-12 23:28:39 +0600

    own component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import AmarComponent from '../mycomponents/AmarComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <AmarComponent></AmarComponent>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
